4831ba97be99f72a4549ffa3a4ace8ee4335c8cb
Dawid33 <dawidsobczak@fastmail.com>
2021-07-08 17:43:26 +0100

    Added curses initialization and input handling

--- src/main.c
+++ src/main.c
@@ -1,3 +1,23 @@
-int main () {
+#include <stdlib.h>
+#include <curses.h>
+#include "console.h"
+
+void exit_main() {
+    close_console();
+    exit(0);
+}
+
+int main (int argc, char **argv) {
+    //Initialisation code.
+    if (atexit(exit_main) != 0) return -1; // Run function at program exit.
+
+    if (init_console() != 0){
+        close_console();
+        return -1;
+    }
+
+    ConsoleFlags* flags = parse_args(argc, argv);
+    if (flags == NULL) return -1;
+
     return 0;
 }
